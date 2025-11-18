7bd45464de313fb568ca1b1e0ce4857b5117fbf4
Caio Durks <caiodurks@gmail.com>
2025-11-18 01:07:07 -0300

    refactor: ajustar autenticador

--- lib/viewmodel/auth_viewmodel.dart
+++ lib/viewmodel/auth_viewmodel.dart
@@ -2,47 +2,43 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:trabalho_flutter/model/auth_repository.dart';
 
-class AuthViewModel extends AsyncNotifier<User?> {
+class AuthViewModel extends StreamNotifier<User?> {
   @override
-  Future<User?> build() async {
-    return ref.watch(authRepositoryProvider).authState.first;
+  Stream<User?> build() {
+    return ref.watch(authRepositoryProvider).authState;
   }
 
   Future<void> login(String email, String password) async {
     final repo = ref.read(authRepositoryProvider);
-
-    state = const AsyncLoading();
-
-    state = await AsyncValue.guard(() async {
-      final credential = await repo.signIn(email, password);
-      return credential.user;
-    });
+    
+    try {
+      await repo.signIn(email, password);
+    } catch (e) {
+      rethrow;
+    }
   }
 
   Future<void> signup(String name, String email, String password) async {
     final repo = ref.read(authRepositoryProvider);
 
-    state = const AsyncLoading();
-
-    state = await AsyncValue.guard(() async {
-      final credential = await repo.signUp(name, email, password);
-      return credential.user;
-    });
+    try {
+      await repo.signUp(name, email, password);
+    } catch (e) {
+      rethrow;
+    }
   }
 
   Future<void> logout() async {
     final repo = ref.read(authRepositoryProvider);
 
-    state = const AsyncLoading();
-
-    state = await AsyncValue.guard(() async {
+    try {
       await repo.signOut();
-      return null; 
-    });
+    } catch (e) {
+      rethrow;
+    }
   }
 }
 
-final authViewModelProvider = AsyncNotifierProvider<AuthViewModel, User?>(
+final authViewModelProvider = StreamNotifierProvider<AuthViewModel, User?>(
   AuthViewModel.new,
 );
-
